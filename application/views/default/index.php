3a57e97fb1ff2a017c39e6a92e00acf8d809f07f
alongken2005 <360568201@qq.com>
2013-10-22 00:14:06 +0800

    更新

--- application/views/default/index.php
+++ application/views/default/index.php
@@ -1,14 +1,20 @@
 <?php $this->load->view(THEME.'/header');?>
+<link rel="stylesheet" type="text/css" href="<?=THEME_VIEW?>css/index.css"/>
 <div class="box">
+	<div class="slider_title">Continue Reading for Jerry</div>
 	<div class="slider_box">
 		<a href="" class="pre"></a>
-		<a href="" class="next"></a>
-		<div class="slider_title">Continue Reading for Jerry</div>
 		<div class="slider_imgs">
 			<ul>
-				<li><img src=""/></li>
+			<?php for($i = 1; $i<8; $i++) {?>
+				<li>
+					<img src="data/12.jpg"/>
+					<div><?="好吧".$i?></div>
+				</li>
+			<?php }?>
 			</ul>
 		</div>
+		<a href="" class="next"></a>
 	</div>
 </div>
 <?php $this->load->view(THEME.'/footer');?>
