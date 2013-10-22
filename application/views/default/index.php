4c8c8629e7a7dc015536a4b191f925b8ba5cec40
alongken2005 <360568201@qq.com>
2013-10-22 23:52:32 +0800

    更新

--- application/views/default/index.php
+++ application/views/default/index.php
@@ -1,12 +1,47 @@
 <?php $this->load->view(THEME.'/header');?>
 <link rel="stylesheet" type="text/css" href="<?=THEME_VIEW?>css/index.css"/>
+<div class="cover_float">
+	
+</div>
 <div class="box">
 	<div class="slider_title">Continue Reading for Jerry</div>
-	<div class="slider_box">
+	<div class="read_his slider_box">
 		<a href="" class="pre"></a>
 		<div class="slider_imgs">
-			<ul>
-			<?php for($i = 1; $i<8; $i++) {?>
+			<ul class="sliderul">
+			<?php for($i = 1; $i<10; $i++) {?>
+				<li>
+					<img src="data/12.jpg"/>
+					<div><?="好吧".$i?></div>
+				</li>
+			<?php }?>
+			</ul>
+		</div>
+		<a href="" class="next"></a>
+	</div>
+
+	<div class="slider_title">My List</div>
+	<div class="my_list slider_box">
+		<a href="" class="pre"></a>
+		<div class="slider_imgs">
+			<ul class="sliderul">
+			<?php for($i = 1; $i<10; $i++) {?>
+				<li>
+					<img src="data/12.jpg"/>
+					<div><?="好吧".$i?></div>
+				</li>
+			<?php }?>
+			</ul>
+		</div>
+		<a href="" class="next"></a>
+	</div>
+
+	<div class="slider_title">Top Picks for Jerry</div>
+	<div class="top_picks slider_box">
+		<a href="" class="pre"></a>
+		<div class="slider_imgs">
+			<ul class="sliderul">
+			<?php for($i = 1; $i<10; $i++) {?>
 				<li>
 					<img src="data/12.jpg"/>
 					<div><?="好吧".$i?></div>
@@ -17,4 +52,44 @@
 		<a href="" class="next"></a>
 	</div>
 </div>
+
+<script type="text/javascript">
+	$(function() {
+		sliderMove('.read_his');
+		sliderMove('.my_list');
+		sliderMove('.top_picks');
+
+		$('.slider_box li').hover(function() {
+
+		});
+	})
+
+	function sliderMove(selected) {
+		var ulwidth = $(selected+' .sliderul li').length*130;
+		$(selected+' .sliderul').width(ulwidth);
+
+		$(selected).hover(function() {
+			$(selected+' .pre, '+selected+' .next').show();
+		}, function() {
+			$(selected+' .pre, '+selected+' .next').hide();
+		});
+
+		$(selected+' .pre').hover(function() {
+			$(selected+' .sliderul').animate( { left: "0px"}, { queue: false, duration: 'slow' } );
+			return false;
+		}, function() {
+			$(selected+' .sliderul').stop();
+		})
+
+		$(selected+' .next').hover(function() {
+			var leftvar = 1000-ulwidth;
+			if(leftvar < 0) {
+				$(selected+' .sliderul').animate( { left: leftvar+"px"}, { queue: false, duration: 'slow' } );
+			}
+			return false;
+		}, function() {
+			$(selected+' .sliderul').stop();
+		})
+	}
+</script>
 <?php $this->load->view(THEME.'/footer');?>
